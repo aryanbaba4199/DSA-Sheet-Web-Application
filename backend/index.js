1b6b3e198906fd8737284870772c285d368e8a0d
aryanbaba4199 <aryanbaba4199@gmail.com>
2025-09-24 18:05:54 +0530

    clien uri update

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://assignment-frontend-39141625808.asia-south1.run.app", 
     credentials: true,               
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
